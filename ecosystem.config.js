9b250642bfa4cec03889415792b71b13e4f8de43
Mohammad Dandy Putra <putradandy619@gmail.com>
2024-08-25 18:09:01 +0700

    install pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      interpreter: 'ts-node',
+    },
+  ],
+};
